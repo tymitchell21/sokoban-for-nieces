90c43b27d82c4db35c978e0540598b7a7f143172
Tyler <tymitchell2100@gmail.com>
2018-12-04 10:52:16 -0500

    initial commit

---
+++ links/main.js
@@ -0,0 +1,11 @@
+const  map = [  
+    "  WWWWW ",  
+    "WWW   W ",  
+    "WOSB  W ",  
+    "WWW BOW ",  
+    "WOWWB W ",  
+    "W W O WW",  
+    "WB XBBOW",  
+    "W   O  W",  
+    "WWWWWWWW"  
+  ];
